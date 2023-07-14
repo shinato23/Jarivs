868a0cb64aa9357d95ec827d6d4662c521d4eb64
shinato23 <104008257+shinato23@users.noreply.github.com>
2023-07-14 20:41:09 +0700

    Add files via upload

---
+++ Jarvis_completed/command.groovy
@@ -0,0 +1,11 @@
+Vietnamese:
+Các bước ra lệnh:
+Bước 1: nói computer hoặc Jarvis
+Bước 2: + tên hành động + trang web
+    + "play" phát video youtube + tên video
+    + "say" hello hoặc time để chào hoặc hẹn giờ
+    + "speak" + bất kỳ cái gì muốn hỏi chat gpt
+    + "computer" hoặc "compute" + thứ mình muốn tính toán
+    + "search" + thứ mình muốn tìm trên gg
+    + "go" + "to" + duong link lien ket de truc tiep truy cap trang web ( VD facebook.com)
+    + "wikipedia" + 1 từ đại diện muốn hỏi (VD wikipedia + Python để hỏi về Python)
